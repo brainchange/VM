2cb874b6a82fd5e00c2ed55aec444d54db05be0f
kaustav1996 <kaustavsmailbox21@gmail.com>
2017-08-27 04:53:19 +0530

    h

--- main.sh
+++ main.sh
@@ -164,7 +164,7 @@ install_desktop()
 automation_kit()
 {
 	sudo apt -y install python-pip git
-	sudo pip install selenium
+	sudo pip install selenium bs4
 	cd /home
 	sudo apt-get -f install
 	sudo git clone https://github.com/brainchange/Automation
